88c952bdfe9e1cf38436e424a88696414654c5ea
kai <gmdezreal@gmail.com>
2022-10-25 15:40:49 -0500

    set up the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+// main.rs
+// cli handling
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Args 
+{
+    /// Title of the notification
+    #[arg(short = 't', long = "title", default_value_t = String::new())]
+    notif_title: String
+}
+
+fn main() 
+{
+    let args = Args::parse();
+
+    println!("Title of notification: {}", args.notif_title);
 }
